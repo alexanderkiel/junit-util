a8104eb07097644f48984e83c2d119d36fac5620
Alexander Kiel <alexanderkiel@gmx.net>
2010-07-13 13:34:47 +0000

    removed abstract keyword because it is a bit odd to have this class abstract the private constructor is sufficient

--- src/main/java/net/alexanderkiel/junit/Assert.java
+++ src/main/java/net/alexanderkiel/junit/Assert.java
@@ -25,52 +25,52 @@ import static org.junit.Assert.fail;
  * @author Alexander Kiel
  * @version $Id$
  */
-public abstract class Assert {
+public class Assert {
 
-    private Assert() {
-    }
+	private Assert() {
+	}
 
-    /**
-     * Asserts that the equals and hashCode methods of the class under test behave normally.
-     * <p/>
-     * This method tests six things: <ul> <li>that equals is reflexive, <li>that a comparison with {@code null} returns
-     * {@code false}, <li>that a comparison with another class returns {@code false}, <li>that equals is symmetric,
-     * <li>that hashCode of the two equal instances returns the same value and <li>that hashCode of the two different
-     * instances returns different values </ul>
-     * <p/>
-     * This method needs three non-null instances of the class under test, whereas the first two should be equal and the
-     * last should not be equal to any of the first two.
-     *
-     * @param foo1 the first of two equal instances
-     * @param foo2 the second of two equal instances
-     * @param bar  an instance not equals to any of the first two
-     * @param <T>  the type of the class under test
-     * @see Object#equals(Object)
-     * @see Object#hashCode()
-     */
-    @SuppressWarnings({"ObjectEqualsNull"})
-    public static <T> void assertBasicEqualsAndHashCodeBehavior(T foo1, T foo2, T bar) {
-        assertNotNull("foo1 is not null", foo1);
-        assertNotNull("foo1 is not null", foo2);
-        assertNotNull("bar is not null", bar);
+	/**
+	 * Asserts that the equals and hashCode methods of the class under test behave normally.
+	 * <p/>
+	 * This method tests six things: <ul> <li>that equals is reflexive, <li>that a comparison with {@code null} returns
+	 * {@code false}, <li>that a comparison with another class returns {@code false}, <li>that equals is symmetric,
+	 * <li>that hashCode of the two equal instances returns the same value and <li>that hashCode of the two different
+	 * instances returns different values </ul>
+	 * <p/>
+	 * This method needs three non-null instances of the class under test, whereas the first two should be equal and the
+	 * last should not be equal to any of the first two.
+	 *
+	 * @param foo1 the first of two equal instances
+	 * @param foo2 the second of two equal instances
+	 * @param bar  an instance not equals to any of the first two
+	 * @param <T>  the type of the class under test
+	 * @see Object#equals(Object)
+	 * @see Object#hashCode()
+	 */
+	@SuppressWarnings({"ObjectEqualsNull", "SimplifiableJUnitAssertion", "ProhibitedExceptionCaught"})
+	public static <T> void assertBasicEqualsAndHashCodeBehavior(T foo1, T foo2, T bar) {
+		assertNotNull("foo1 is not null", foo1);
+		assertNotNull("foo1 is not null", foo2);
+		assertNotNull("bar is not null", bar);
 
-        assertTrue("equals is reflexive", foo1.equals(foo1));
+		assertTrue("equals is reflexive", foo1.equals(foo1));
 
-        try {
-            assertFalse("not equals null", foo1.equals(null));
-        } catch (NullPointerException e) {
-            fail("equals doesn't throw NullPointerExceptions");
-        }
+		try {
+			assertFalse("not equals null", foo1.equals(null));
+		} catch (NullPointerException e) {
+			fail("equals doesn't throw NullPointerExceptions");
+		}
 
-        assertFalse("not equals instances other classes", foo1.equals(new Object()));
+		assertFalse("not equals instances other classes", foo1.equals(new Object()));
 
-        assertTrue("equals is symmetric", foo1.equals(foo2));
-        assertTrue("equals is symmetric", foo2.equals(foo1));
+		assertTrue("equals is symmetric", foo1.equals(foo2));
+		assertTrue("equals is symmetric", foo2.equals(foo1));
 
-        assertFalse("equals is symmetric", foo2.equals(bar));
-        assertFalse("equals is symmetric", bar.equals(foo2));
+		assertFalse("equals is symmetric", foo2.equals(bar));
+		assertFalse("equals is symmetric", bar.equals(foo2));
 
-        assertTrue(foo1.hashCode() == foo2.hashCode());
-        assertTrue(foo2.hashCode() != bar.hashCode());
-    }
+		assertTrue("hash code of equal objects is equal too", foo1.hashCode() == foo2.hashCode());
+		assertTrue("hash code of unequal objects is not equal", foo2.hashCode() != bar.hashCode());
+	}
 }
